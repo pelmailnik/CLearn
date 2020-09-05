c83918b2a079104f15723efd52a1e84174541fb1
pelmailnik <pelmailnik@gmail.com>
2020-09-05 12:47:03 +0300

    Check input with regular expression

--- StrCalc/Program.cs
+++ StrCalc/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace StrCalc
 {
+    class Calculator
+    {
+        private string exp;
+
+       
+        public void GetExpression()
+        {
+            Console.WriteLine("Enter your expression:");
+            do
+            {
+                exp = Console.ReadLine();
+            } while (IsIncorrect());
+
+            //Console.WriteLine(exp);
+        }
+
+        public bool IsIncorrect()
+        {
+            string pattern = "^([^\\*\\/]{0,1}[0-9]{1,})(([\\+\\-\\*\\/\\(\\)]{0,1}[0-9]){1,})$";   //победить скобка + знак
+            if (Regex.IsMatch(exp, pattern, RegexOptions.IgnoreCase))
+            {
+                Console.WriteLine("NORM");
+                return true;                                //дебаг
+                //return false;                             //правильно
+            }
+            Console.WriteLine("Incorrect input! Try again:");
+            return true;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Calculator obj = new Calculator();
+            obj.GetExpression();
+            
         }
     }
 }
