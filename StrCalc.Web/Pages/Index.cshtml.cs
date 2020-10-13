a84caf1018f05f5af40351b096d013715091a96a
pelmailnik <pelmailnik@gmail.com>
2020-10-13 16:46:05 +0300

    Реорганизация

---
+++ StrCalc.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace StrCalc.Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public string expressoin { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnPost(string exp)
+        {
+            var expressionValidator = new ExpressionValidator();
+            ICalculator obj = new Calculator(expressionValidator);
+
+            if (!String.IsNullOrEmpty(exp))
+            {
+                expressoin = obj.Calculate(exp).ToString();
+            }
+        }
+    }
+}
